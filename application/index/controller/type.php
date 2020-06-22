85886e7d433bfa22fc70926e8a8681f25a1d45ff
xiaohan1995 <1192539415@qq.com>
2020-06-22 23:33:23 +0800

    get

---
+++ application/index/controller/type.php
@@ -0,0 +1,43 @@
+<?php
+namespace app\index\controller;
+use think\Db;
+use think\Controller;
+
+
+class Type extends Controller
+{   
+	
+    public function getcate()
+    {
+        $data = Db::table('yx_goodtype')->where('t_status=1')->select();
+        if(!empty($data)){
+        	api_success($data);
+        }else{
+        	api_success();
+        }
+    }
+
+    
+    
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
