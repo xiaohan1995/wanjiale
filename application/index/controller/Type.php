581821afef84e957a215c249e14017f415a03609
xiaohan1995 <1192539415@qq.com>
2020-06-23 00:00:49 +0800

    add

---
+++ application/index/controller/Type.php
@@ -0,0 +1,55 @@
+<?php
+namespace app\index\controller;
+use think\Db;
+use think\Controller;
+
+
+class Type extends Controller
+{   
+    
+    public function getcate()
+    {
+        $data = Db::table('yx_goodtype')->where('t_status=1')->select();
+        if(!empty($data)){
+            api_success($data);
+        }else{
+            api_success();
+        }
+    }
+
+    public function getgoods(){
+        $input = input();
+        $data = Db::table('yx_goods')
+                ->where('g_type='.$input['t_id'].' and g_status=1')
+                ->select();
+        if(!empty($data)){
+            api_success($data);
+        }else{
+            api_success();
+        }
+    }
+
+    
+    
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
